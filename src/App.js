49e9de71e1c1a6da4030570929d758920c60c834
Chenk14 <chenk@wix.com>
2022-03-24 18:20:27 +0200

    fixing double resource call

--- src/App.js
+++ src/App.js
@@ -5,62 +5,89 @@ import { Multiselect } from "multiselect-react-dropdown";
 import { Fragment, useState } from "react";
 import CollapsableSection from "./CollapsableSection";
 
-
-
 const App = ({ entries }) => {
-
   const google = useGoogleCharts();
   const types = [
-    { name: "resource", id: 1, description:'Representing a fetch call the fetch a resource, such as fetch the dynamic model' },
-    { name: "mark", id: 2, description:'Representing the marking of a stage, such as the start and end of load_renderer' },
-    { name: "measure", id: 3, description:'Representing a measurement' },
-    { name: "paint", id: 4, description:'Representing a paint stage' },
-    { name: "navigation", id: 5, description:'Representing the navigation call' },
-    { name: "first-input", id: 6, description:''},
-    { name: "frog calls", id: 7, description:'Representing a fetch call to a frog.wix domains, it is not shown by default since it doesnt affect the loading time' },
-    { name: "cloudflare", id: 8, description:'Representing a fetch call to a cloudflare domains, it is not shown by default since it doesnt affect the loading time' },
+    {
+      name: "resource",
+      id: 1,
+      description:
+        "Representing a fetch call the fetch a resource, such as fetch the dynamic model",
+    },
+    {
+      name: "mark",
+      id: 2,
+      description:
+        "Representing the marking of a stage, such as the start and end of load_renderer",
+    },
+    { name: "measure", id: 3, description: "Representing a measurement" },
+    { name: "paint", id: 4, description: "Representing a paint stage" },
+    {
+      name: "navigation",
+      id: 5,
+      description: "Representing the navigation call",
+    },
+    { name: "first-input", id: 6, description: "" },
+    {
+      name: "frog calls",
+      id: 7,
+      description:
+        "Representing a fetch call to a frog.wix domains, it is not shown by default since it doesnt affect the loading time",
+    },
+    {
+      name: "cloudflare",
+      id: 8,
+      description:
+        "Representing a fetch call to a cloudflare domains, it is not shown by default since it doesnt affect the loading time",
+    },
   ];
-  const [filters, setFilters] = useState(types.filter(filter=>filter.id!==7&&filter.id!==8));
+  const [filters, setFilters] = useState(
+    types.filter((filter) => filter.id !== 7 && filter.id !== 8)
+  );
 
   const onSelect = (selectedList, _selectedItem) => {
     setFilters(selectedList);
-  }
+  };
   const onRemove = (selectedList, _removedItem) => {
     setFilters(selectedList);
-  }
+  };
 
-    const timeline=document.getElementById('container')
-    if(timeline){
-      timeline.onmousemove = (event)=> {
-        const line= document.getElementById('line')
-        if(line){
-          line.style.position = "absolute";
-          line.style.left = (event.pageX-3)+'px';
-        }
+  const timeline = document.getElementById("container");
+  if (timeline) {
+    timeline.onmousemove = (event) => {
+      const line = document.getElementById("line");
+      if (line) {
+        line.style.position = "absolute";
+        line.style.left = event.pageX - 3 + "px";
       }
-    }
+    };
+  }
 
   return (
     <>
-          <CollapsableSection types={types}/>
-          <Multiselect
-            options={types} // Options to display in the dropdown
-            selectedValues={filters} // Preselected value to persist in dropdown
-            onSelect={onSelect} // Function will trigger on select event
-            onRemove={onRemove} // Function will trigger on remove event
-            displayValue="name" // Property name to display in the dropdown options
-            placeholder='Please select filter'
-            showCheckbox={true}
-            closeOnSelect={true}
-            className='basic_components'
-          />
-          <div className='container' id='container'>
-              <div className='line' id='line'/>
-            <PerformanceTimeline className='timeline' google={google} filters={filters} entries={entries}/>
-          </div>
+      <CollapsableSection types={types} />
+      <Multiselect
+        options={types} // Options to display in the dropdown
+        selectedValues={filters} // Preselected value to persist in dropdown
+        onSelect={onSelect} // Function will trigger on select event
+        onRemove={onRemove} // Function will trigger on remove event
+        displayValue="name" // Property name to display in the dropdown options
+        placeholder="Please select filter"
+        showCheckbox={true}
+        closeOnSelect={true}
+        className="basic_components"
+      />
+      <div className="container" id="container">
+        <div className="line" id="line" />
+        <PerformanceTimeline
+          className="timeline"
+          google={google}
+          filters={filters}
+          entries={entries}
+        />
+      </div>
     </>
   );
 };
 
 export default App;
-
--- src/PerformanceTimeline.js
+++ src/PerformanceTimeline.js
@@ -5,34 +5,37 @@ function PerformanceTimeline({ google, filters, entries }) {
   const [timeline, setTimeline] = useState(null);
   const [currentFilters, setCurrentFilters] = useState(filters);
   useEffect(() => {
-    if (google && (!timeline|| currentFilters !== filters)) {
+    if (google && (!timeline || currentFilters !== filters)) {
       google.charts.load("current", { packages: ["timeline"] });
       google.charts.setOnLoadCallback(() =>
-        drawChart(filters, google, setTimeline,entries)
-      )
-      setCurrentFilters(filters)
+        drawChart(filters, google, setTimeline, entries)
+      );
+      setCurrentFilters(filters);
     }
   }, [currentFilters, entries, filters, google, timeline]);
 
   return (
     <div>
       {!google && <Spinner />}
-      <div id="timeline"  />
+      <div id="timeline" />
     </div>
   );
 }
 
 export default PerformanceTimeline;
 
-const getPerformanceObject = async (filters,entries) => {
-  const data = entries
+const getPerformanceObject = async (filters, entries) => {
+  const data = entries;
   let retList = [];
-  filters=filters.map((filter) => filter.name)
+  filters = filters.map((filter) => filter.name);
   data.forEach((entry) => {
     if (filterEntries(entry, filters)) {
       const retEntry = {
         displayName: entry.name,
-        type: entry.entryType==='resource'?entry.initiatorType:entry.entryType,
+        type:
+          entry.entryType === "resource"
+            ? entry.initiatorType
+            : entry.entryType,
         name: entry.name,
         starts: entry.startTime,
         ends: entry.startTime + entry.duration,
@@ -56,7 +59,7 @@ const getPerformanceObject = async (filters,entries) => {
 
       let found = false;
       retList.forEach((ret) => {
-        if (ret.name === retEntry.name) {
+        if (ret.name === retEntry.name && entry.entryType!=='resource') {
           ret.ends = entry.startTime + entry.duration;
           found = true;
         }
@@ -72,11 +75,11 @@ const getPerformanceObject = async (filters,entries) => {
   return retList;
 };
 
-const drawChart = async (filters, google, setTimeline,entries) => {
+const drawChart = async (filters, google, setTimeline, entries) => {
   const container = document.getElementById("timeline");
   const newChart = new google.visualization.Timeline(container);
   const dataTable = new google.visualization.DataTable();
-  const data = await getPerformanceObject(filters,entries);
+  const data = await getPerformanceObject(filters, entries);
   dataTable.addColumn({ type: "string", id: "row label" });
   dataTable.addColumn({ type: "string", id: "bar label" });
   dataTable.addColumn({ type: "string", role: "tooltip" });
@@ -104,43 +107,39 @@ const drawChart = async (filters, google, setTimeline,entries) => {
   const options = {
     timeline: { colorByRowLabel: true, groupByRowLabel: false },
   };
-  console.log(dataTable.getNumberOfRows())
-  container.style.height = `${dataTable.getNumberOfRows() * 43.2 }px`;
+  console.log(dataTable.getNumberOfRows());
+  container.style.height = `${dataTable.getNumberOfRows() * 43.2}px`;
   newChart.draw(dataTable, options);
   //container.style.height = dataTable.getNumberOfRows() * 15+40;
   setTimeline(newChart);
 };
 
-
-
-const getDisplayNameOfLoadedResource=(name)=>{
-  if(name.includes('siteassets')){
-    const start= name.search('module=')+7
-    const end=name.slice(start).search('&') + start
-    return `Site Assets (${name.slice(start, end)})`
-  }
-  else if(name.endsWith('.js') || name.endsWith('dynamicmodel') ){
+const getDisplayNameOfLoadedResource = (name) => {
+  if (name.includes("siteassets")) {
+    const start = name.search("module=") + 7;
+    const end = name.slice(start).search("&") + start;
+    return `Site Assets (${name.slice(start, end)})`;
+  } else if (name.endsWith(".js") || name.endsWith("dynamicmodel")) {
     let fileSuffixIndex = -1;
     for (let i = 0; i < name.length; i++)
-      if (name[i] === '/')
-        fileSuffixIndex = i;
+      if (name[i] === "/") fileSuffixIndex = i;
 
-    return name.slice(fileSuffixIndex+1,name.length)
+    return name.slice(fileSuffixIndex + 1, name.length);
   }
-return 'fetch call'
-}
+  return "fetch call";
+};
 
-const filterEntries = (entry,filters)=>{
+const filterEntries = (entry, filters) => {
   return (
-  !entry.name.includes("pointerdown")&&
-  !entry.name.includes("thunderbolt interaction")&&
-  !entry.name.includes("phase:")&&
-  !entry.name.endsWith("duration") &&
-  !entry.name.startsWith("@grammarly") &&
-  filters.includes(entry.entryType)&&
-  ((filters.includes('frog calls'))||
-    (!filters.includes('frog calls')&&!entry.name.includes('frog')))&&
-  ((filters.includes('cloudflare'))||
-    (!filters.includes('cloudflare')&&!entry.name.includes('cloudflare')))
-  )
-}
+    !entry.name.includes("pointerdown") &&
+    !entry.name.includes("thunderbolt interaction") &&
+    !entry.name.includes("phase:") &&
+    !entry.name.endsWith("duration") &&
+    !entry.name.startsWith("@grammarly") &&
+    filters.includes(entry.entryType) &&
+    (filters.includes("frog calls") ||
+      (!filters.includes("frog calls") && !entry.name.includes("frog"))) &&
+    (filters.includes("cloudflare") ||
+      (!filters.includes("cloudflare") && !entry.name.includes("cloudflare")))
+  );
+};
